45875320d6ea9c21a1ae998ed5c648efb3632957
nyuwaea <luniwae@gmail.com>
2026-06-03 22:41:11 -0400

    feat: Strobe modifier

---
+++ scripts/game/modifiers/StrobeModifier.cs
@@ -0,0 +1,18 @@
+﻿using Godot;
+
+public class StrobeModifier : Modifier, IObjectRenderModifier<Note>
+{
+    public override string Name => "Strobe";
+
+    public override bool Rankable => true;
+
+    public override double ScoreMultiplier => 1.035;
+
+    public void ModifyRenderObject(Note note, float depth, Attempt attempt)
+    {
+        if ((attempt.Progress / 1000 / attempt.Speed) % 0.5 > 0.25)
+        {
+            note.Opacity = 0;
+        }
+    }
+}
