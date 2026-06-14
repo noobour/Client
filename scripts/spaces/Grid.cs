f5b874194e57680fbb93173cca37d7142d8a2722
noobour <noobisnotbad1@gmail.com>
2026-06-14 00:21:07 -0400

    Feat: Relic space + Cube, Plane mesh
    adds a new space based on legacy's intro, and reintroduces both the cube and plane mesh. cube mesh was slightly altered with *hopefully* more transparency to increase it's playability

--- scripts/spaces/Grid.cs
+++ scripts/spaces/Grid.cs
@@ -4,20 +4,20 @@ namespace Spaces;
 
 public partial class Grid : BaseSpace
 {
-    private StandardMaterial3D tileMaterial;
+	private StandardMaterial3D tileMaterial;
 
-    public override void _Ready()
-    {
-        base._Ready();
+	public override void _Ready()
+	{
+		base._Ready();
 
-        tileMaterial = (GetNode<MeshInstance3D>("Top").Mesh as PlaneMesh).Material as StandardMaterial3D;
-    }
+		tileMaterial = (GetNode<MeshInstance3D>("Top").Mesh as PlaneMesh).Material as StandardMaterial3D;
+	}
 
-    public override void _Process(double delta)
-    {
-        base._Process(delta);
+	public override void _Process(double delta)
+	{
+		base._Process(delta);
 
-        tileMaterial.AlbedoColor = NoteHitColor;
-        tileMaterial.Uv1Offset += Vector3.Up * (float)delta * 3;
-    }
+		tileMaterial.AlbedoColor = NoteHitColor;
+		tileMaterial.Uv1Offset += Vector3.Up * (float)delta * 3;
+	}
 }
