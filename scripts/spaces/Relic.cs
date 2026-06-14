7a692edc0f847c6717c86f45a7db29d57d515a8f
noobour <noobisnotbad1@gmail.com>
2026-06-14 15:15:39 -0400

    fixing format and adding myself to the contributor list
    yerp

--- scripts/spaces/Relic.cs
+++ scripts/spaces/Relic.cs
@@ -2,17 +2,4 @@ using Godot;
 
 namespace Spaces;
 
-public partial class Relic : BaseSpace
-{
-
-	public override void _Ready()
-	{
-		base._Ready();
-
-	}
-
-	public override void _Process(double delta)
-	{
-		base._Process(delta);
-	}
-}
+public partial class Relic : BaseSpace { }
