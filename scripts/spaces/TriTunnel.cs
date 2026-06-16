aa8de4a00a085338c87dd17e68083d5fcb476020
noobour <noobisnotbad1@gmail.com>
2026-06-15 23:25:33 -0400

    fix some stuff
    yerp

--- scripts/spaces/TriTunnel.cs
+++ scripts/spaces/TriTunnel.cs
@@ -2,42 +2,40 @@ using Godot;
 
 namespace Spaces;
 
-public partial class tritunnel : BaseSpace
+public partial class TriTunnel : BaseSpace
 {
     private SettingsProfile settings;
     private MeshInstance3D tunnel;
     private StandardMaterial3D tunnelTexture;
-    private const float tunnel_loop_end = 148f;
     private Vector3 tunnelPosReset;
     private Color tunnelColorReset = new(255, 255, 255);
+    private const float tunnel_loop_end = 148f;
 
     public override void _Ready()
     {
         base._Ready();
+
         settings = SettingsManager.Instance.Settings;
         tunnel = GetNode<MeshInstance3D>("Tunnel");
         tunnelPosReset = tunnel.Position;
         tunnelTexture = tunnel.MaterialOverride as StandardMaterial3D;
-
     }
 
     public override void _Process(double delta)
     {
         base._Process(delta);
 
-
         // Tunnel movement
         if (tunnel.Position.Z < tunnel_loop_end)
         {
-            tunnel.Position += Vector3.Back * (float)(settings.ApproachRate * (float)delta / 6);
+            tunnel.Position += Vector3.Back * (float)(settings.ApproachRate * delta / 6);
         }
         else
         {
             tunnel.Position = tunnelPosReset;
         }
-        ;
 
-        // hit sfx
+        // Hit VFX
         tunnelTexture.AlbedoColor = settings.SpaceHitEffects ? NoteHitColor : tunnelColorReset;
     }
 }
