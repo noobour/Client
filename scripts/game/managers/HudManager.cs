daadfad263932c5925f2593821778b1c6e9a2c2a
nyarw <github@xwalfie.dev>
2026-08-30 02:37:38 +0200

    feat: show modifier icons below speed

--- scripts/game/managers/HudManager.cs
+++ scripts/game/managers/HudManager.cs
@@ -1,16 +1,28 @@
 using System.Collections.Generic;
 using Godot;
+using Util;
 
 public partial class HudManager : Node
 {
     [Export] public Runner Runner;
 
     private List<IUIComponent> components = [];
+    private readonly List<Sprite3D> modifierIcons = [];
+    private Sprite3D modifierTemplate;
 
     public void Init()
     {
         Runner ??= GetParent<Runner>();
         components = findAllComponents(this);
+
+        modifierTemplate ??= GetNode<Sprite3D>("TemplateModifier");
+        modifierTemplate.Visible = false;
+
+        foreach (var icon in modifierIcons)
+        {
+            icon.QueueFree();
+        }
+        modifierIcons.Clear();
 
         foreach (var component in components)
         {
@@ -21,8 +33,22 @@ public partial class HudManager : Node
 
     public void DisplayModifier(Modifier mod)
     {
-        // TODO: display activated modifiers & clear on Init
-        GD.Print($"HUD: display {mod.Name}");
+        Sprite3D icon = (Sprite3D)modifierTemplate.Duplicate();
+        icon.Texture = Misc.GetModIcon(mod.Name);
+        icon.Visible = true;
+        modifierTemplate.GetParent().AddChild(icon);
+        modifierIcons.Add(icon);
+        positionModifiers();
+    }
+
+    private void positionModifiers()
+    {
+        for (int i = 0; i < modifierIcons.Count; i++)
+        {
+            Vector3 pos = modifierTemplate.Position;
+            pos.X = (i - (modifierIcons.Count - 1) / 2f) * 0.75f;
+            modifierIcons[i].Position = pos;
+        }
     }
 
     public override void _Process(double delta)
